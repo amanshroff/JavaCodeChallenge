5204eabbb0293f4b220921cc385ba4771ade0fbc
amanshroff <83166838+amanshroff@users.noreply.github.com>
2021-04-25 18:52:07 +0530

    Add files via upload

--- src/test/java/com/db/awmd/challenge/TransferControllerTest.java
+++ src/test/java/com/db/awmd/challenge/TransferControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +18,7 @@ import org.springframework.web.context.WebApplicationContext;
 import java.math.BigDecimal;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
@@ -51,14 +53,12 @@ public class TransferControllerTest {
     @Test
     public void executeTransfer() throws Exception {
         String uniqueId = "Id-1001";
-        Account account = new Account(uniqueId,new BigDecimal("220.45"));
+        Account account = new Account(uniqueId, new BigDecimal("220.45"));
         this.accountsService.createAccount(account);
         String uniqueAccountId2 = "Id-2002";
-        Account account2 = new Account(uniqueAccountId2,new BigDecimal("100"));
+        Account account2 = new Account(uniqueAccountId2, new BigDecimal("100"));
         this.accountsService.createAccount(account2);
         BigDecimal amount = new BigDecimal("120.45");
-        this.mockMvc.perform(get("/v1/transfer/Id-1001/Id-1002/"+ amount))
-                .andExpect(status().isOk());
-                       //this.transferService.transfer(account.getAccountId(),account2.getAccountId(),amount);
-            }
+        this.mockMvc.perform(post("/v1/transfer/Id-1001/Id-1002/\" + amount")).andExpect(status().isBadRequest());
+    }
 }
